567609a100c7b4da84fb33911df72bb1ace1173f
principles31 <principles31@gmail.com>
2022-06-20 21:47:39 +0000

    Update file

--- src/redux/categories/configureStore.js
+++ src/redux/categories/configureStore.js
@@ -1,6 +1,6 @@
 import { configureStore, combineReducers } from 'redux';
-import bookReducer from '.src/redux/books/books';
-import checkStatusReducer from '.src/redux/categories/categories';
+import bookReducer from './books/books';
+import checkStatusReducer from './categories/categories';
 
 const reducer = combineReducers({
   bookReducer,
